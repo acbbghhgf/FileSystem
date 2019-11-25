e227f657a4f104a6181b95cb795755b97bb1d881
ww <490165508@qq.com>
2019-11-25 01:41:24 -0800

    create ui display

--- rootfs/root/graphic_process/graphic_process/mainwidget.cpp
+++ rootfs/root/graphic_process/graphic_process/mainwidget.cpp
@@ -6,6 +6,8 @@ mainWidget::mainWidget(QWidget *parent)
     , ui(new Ui::mainWidget)
 {
     ui->setupUi(this);
+    graphicsView_width = ui->graphicsView->width();
+    graphicsView_height = ui->graphicsView->height();
 }
 
 mainWidget::~mainWidget()
@@ -13,3 +15,56 @@ mainWidget::~mainWidget()
     delete ui;
 }
 
+
+void mainWidget::on_setHeader_pb_clicked()
+{
+    //设置表头
+        QTableWidgetItem    *headerItem;
+        QStringList headerText;
+        headerText<<"序 号"<<"X1"<<"Y1"<<"X2"<<"Y2"<<"YZ"<<"theshold";  //表头标题用QStringList来表示
+        ui->tableInfo->setHorizontalHeaderLabels(headerText);
+        ui->tableInfo->setColumnCount(headerText.count());//列数设置为与 headerText的行数相等
+        for (int i=0;i<ui->tableInfo->columnCount();i++)//列编号从0开始
+        {
+           headerItem=new QTableWidgetItem(headerText.at(i)); //新建一个QTableWidgetItem， headerText.at(i)获取headerText的i行字符串
+           QFont font=headerItem->font();//获取原有字体设置
+           font.setBold(true);//设置为粗体
+           font.setPointSize(8);//字体大小
+           headerItem->setTextColor(Qt::red);//字体颜色
+           headerItem->setFont(font);//设置字体
+           ui->tableInfo->setHorizontalHeaderItem(i,headerItem); //设置表头单元格的Item
+        }
+
+//        ui->tableInfo->setItemDelegateForColumn(colScore,&spinDelegate);//设置自定义代理组件
+}
+
+void mainWidget::on_exit_pb_clicked()
+{
+    qDebug() << "process exit.";
+    this->close();
+}
+
+void mainWidget::on_select_pb_clicked()
+{
+    pictrue_path = QFileDialog::getOpenFileName(
+                this, "choose src Directory",
+                "/home/ww/");
+
+    if (pictrue_path.isEmpty())
+    {
+        qDebug() << "get filename path error.";
+        return;
+    }
+    else
+    {
+        qDebug() << "get filename path success. filename = " << pictrue_path;
+    }
+    ui->picture_path_edit->setText(pictrue_path);
+    QImage image(pictrue_path, "bmp");
+    QPixmap pixmap = QPixmap::fromImage(image);
+    originalScene.addPixmap(pixmap.scaled(graphicsView_width, graphicsView_height, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    ui->graphicsView->setScene(&originalScene);
+    ui->graphicsView->show();
+
+
+}
--- rootfs/root/graphic_process/graphic_process/mainwidget.h
+++ rootfs/root/graphic_process/graphic_process/mainwidget.h
@@ -2,6 +2,11 @@
 #define MAINWIDGET_H
 
 #include <QWidget>
+#include <QFileDialog>
+#include <QPixmap>
+#include <QGraphicsScene>
+#include <QLabel>
+#include <QDebug>
 #include <opencv2/core.hpp>
 
 QT_BEGIN_NAMESPACE
@@ -16,7 +21,24 @@ public:
     mainWidget(QWidget *parent = nullptr);
     ~mainWidget();
 
+private slots:
+    void on_setHeader_pb_clicked();
+
+    void on_exit_pb_clicked();
+    void on_select_pb_clicked();
+
 private:
     Ui::mainWidget *ui;
+    //  自定义单元格Type的类型，在创建单元格的Item时使用
+    enum    CellType{ctName=1000,ctSex,ctBirth,ctNation,ctPartyM,ctScore}; //各单元格的类型
+    //  各字段在表格中的列号
+    enum    FieldColNum{colName=0, colSex,colBirth,colNation,colScore,colPartyM};
+
+    //picture filename path
+    QString pictrue_path;
+    //picture display
+    QGraphicsScene originalScene;
+    int graphicsView_width;
+    int graphicsView_height;
 };
 #endif // MAINWIDGET_H
