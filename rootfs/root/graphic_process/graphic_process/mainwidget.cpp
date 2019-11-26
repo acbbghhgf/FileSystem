00ab11c9795a62128bb29a83f2a9e5f0d424bfff
ww <490165508@qq.com>
2019-11-26 02:03:20 -0800

    add src picture rect display

--- rootfs/root/graphic_process/graphic_process/mainwidget.cpp
+++ rootfs/root/graphic_process/graphic_process/mainwidget.cpp
@@ -8,6 +8,33 @@ mainWidget::mainWidget(QWidget *parent)
     ui->setupUi(this);
     graphicsView_width = ui->graphicsView->width();
     graphicsView_height = ui->graphicsView->height();
+    timer = new QTimer;
+    connect(timer, SIGNAL(timeout()), this, SLOT(update_display()));
+//    timer->start(1000);
+
+    //init edit
+    QRegExp regx("[0-9]+$");
+    QValidator *validator = new QRegExpValidator(regx);
+    ui->x1_edit->setValidator(validator);
+    ui->x2_edit->setValidator(validator);
+    ui->y1_edit->setValidator(validator);
+    ui->y2_edit->setValidator(validator);
+    ui->X_edit->setValidator(validator);
+    ui->Y_edit->setValidator(validator);
+    ui->yz_edit->setValidator(validator);
+    ui->id_edit->setValidator(validator);
+    ui->width_edit->setValidator(validator);
+    ui->height_edit->setValidator(validator);
+
+    edit_display_x1 = 0;
+    edit_display_x2 = 0;
+    edit_display_y1 = 0;
+    edit_display_y2 = 0;
+    edit_display_X = 0;
+    edit_display_Y = 0;
+    edit_display_yz = 0;
+    edit_display_width = 0;
+    edit_display_height = 0;
 }
 
 mainWidget::~mainWidget()
@@ -15,6 +42,25 @@ mainWidget::~mainWidget()
     delete ui;
 }
 
+
+void mainWidget::update_display()
+{
+    //update edit display
+    ui->x1_edit->setText(QString::number(edit_display_x1));
+    ui->x2_edit->setText(QString::number(edit_display_x2));
+    ui->y1_edit->setText(QString::number(edit_display_y1));
+    ui->y2_edit->setText(QString::number(edit_display_y2));
+    ui->X_edit->setText(QString::number(edit_display_X));
+    ui->Y_edit->setText(QString::number(edit_display_Y));
+    ui->yz_edit->setText(QString::number(edit_display_yz));
+    ui->height_edit->setText(QString::number(edit_display_height));
+    ui->width_edit->setText(QString::number(edit_display_width));
+
+    //update graph view display
+    ui->graphicsView->setScene(&originalScene);
+    ui->graphicsView->show();
+
+}
 
 void mainWidget::on_setHeader_pb_clicked()
 {
@@ -48,7 +94,7 @@ void mainWidget::on_select_pb_clicked()
 {
     pictrue_path = QFileDialog::getOpenFileName(
                 this, "choose src Directory",
-                "/home/ww/");
+                "/home/ww/bmp_demo");
 
     if (pictrue_path.isEmpty())
     {
@@ -60,11 +106,106 @@ void mainWidget::on_select_pb_clicked()
         qDebug() << "get filename path success. filename = " << pictrue_path;
     }
     ui->picture_path_edit->setText(pictrue_path);
-    QImage image(pictrue_path, "bmp");
-    QPixmap pixmap = QPixmap::fromImage(image);
+    src_picture = cv::imread(pictrue_path.toStdString(), 1);
+    process_mid_picture = src_picture.clone();
+    qDebug() << "proce _ channel = " << process_mid_picture.channels();
+    edit_display_x1 = 0;
+    edit_display_x2 = process_mid_picture.cols;
+    edit_display_y1 = 0;
+    edit_display_y2 = process_mid_picture.rows;
+    edit_display_yz = 49;
+    edit_display_width = 40;
+    edit_display_height = 24;
+
+    qDebug() << "proc_cols = " << process_mid_picture.cols << " proc_rows = " << process_mid_picture.rows;
+    m_updatePixmap(process_mid_picture, QImage::Format_RGB888);
+
+    update_display();
+}
+
+
+void mainWidget::m_updatePixmap(cv::Mat &dst, QImage::Format flag)
+{
+
+    originalScene.clear();
+    cv::Mat process = dst.clone();
+    cv::Rect rect(edit_display_x1, edit_display_y1, edit_display_x2 - edit_display_x1, edit_display_y2 - edit_display_y1);
+    cv::rectangle(process, rect, cv::Scalar(255, 0, 0), 1, 8, 0);
+    cv::imwrite("/home/ww/bmp_demo/save.png", process);
+    qDebug() << "proc_cols = " << dst.cols << " proc_rows = " << dst.rows << "graphic width =" << graphicsView_width << " graphic height =" << graphicsView_height;
+    QImage image("/home/ww/bmp_demo/save.png");
+    pixmap = QPixmap::fromImage(image);
     originalScene.addPixmap(pixmap.scaled(graphicsView_width, graphicsView_height, Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    ui->graphicsView->setScene(&originalScene);
-    ui->graphicsView->show();
 
+}
 
+void mainWidget::on_modify_pb_clicked()
+{
+    int tmp_x1, tmp_y1, tmp_width, tmp_height;
+    qDebug() << "x1 " << edit_display_x1 << "x2 " << edit_display_x2 << "y1 " << edit_display_y1 << "y2 " << edit_display_y2;
+    if(edit_display_x1 <= 10)
+        tmp_x1 = edit_display_x1;
+    else
+        tmp_x1 = edit_display_x1 - 10;
+    if(edit_display_y1 <= 10)
+        tmp_y1 = edit_display_y1;
+    else
+        tmp_y1 = edit_display_y1 - 10;
+    if((edit_display_x2 - edit_display_x1) <= 0)
+        tmp_width = 0;
+    else
+        tmp_width = edit_display_x2 - edit_display_x1;
+    if((edit_display_y2 - edit_display_y1) <= 0)
+        tmp_height = 0;
+    else
+        tmp_height = edit_display_y2 - edit_display_y1;
+
+//    process_mid_picture = process_mid_picture(cv::Rect(tmp_x1, tmp_y1, tmp_width, tmp_height));
+    m_updatePixmap(process_mid_picture, QImage::Format_Grayscale8);
+    update_display();
+}
+
+void mainWidget::on_x1_edit_textChanged(const QString &arg1)
+{
+    edit_display_x1 = ui->x1_edit->text().toInt();
+}
+
+void mainWidget::on_y1_edit_textChanged(const QString &arg1)
+{
+    edit_display_y1 = ui->y1_edit->text().toInt();
+}
+
+void mainWidget::on_x2_edit_textChanged(const QString &arg1)
+{
+    edit_display_x2 = ui->x2_edit->text().toInt();
+}
+
+void mainWidget::on_y2_edit_textChanged(const QString &arg1)
+{
+    edit_display_y2 = ui->y2_edit->text().toInt();
+}
+
+void mainWidget::on_yz_edit_textChanged(const QString &arg1)
+{
+    edit_display_yz = ui->yz_edit->text().toInt();
+}
+
+void mainWidget::on_width_edit_textChanged(const QString &arg1)
+{
+    edit_display_width = ui->width_edit->text().toInt();
+}
+
+void mainWidget::on_height_edit_textChanged(const QString &arg1)
+{
+    edit_display_height = ui->height_edit->text().toInt();
+}
+
+void mainWidget::on_X_edit_textChanged(const QString &arg1)
+{
+        edit_display_X = ui->X_edit->text().toInt();
+}
+
+void mainWidget::on_Y_edit_textChanged(const QString &arg1)
+{
+        edit_display_Y = ui->Y_edit->text().toInt();
 }
--- rootfs/root/graphic_process/graphic_process/mainwidget.h
+++ rootfs/root/graphic_process/graphic_process/mainwidget.h
@@ -7,7 +7,10 @@
 #include <QGraphicsScene>
 #include <QLabel>
 #include <QDebug>
+#include <QTimer>
 #include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class mainWidget; }
@@ -26,9 +29,33 @@ private slots:
 
     void on_exit_pb_clicked();
     void on_select_pb_clicked();
+    void update_display();
+
+    void on_modify_pb_clicked();
+
+    void on_x1_edit_textChanged(const QString &arg1);
+
+    void on_y1_edit_textChanged(const QString &arg1);
+
+    void on_x2_edit_textChanged(const QString &arg1);
+
+    void on_y2_edit_textChanged(const QString &arg1);
+
+    void on_yz_edit_textChanged(const QString &arg1);
+
+    void on_width_edit_textChanged(const QString &arg1);
+
+    void on_height_edit_textChanged(const QString &arg1);
+
+    void on_X_edit_textChanged(const QString &arg1);
+
+    void on_Y_edit_textChanged(const QString &arg1);
 
 private:
+    void m_updatePixmap(cv::Mat &dst, QImage::Format flag);
+private:
     Ui::mainWidget *ui;
+    QTimer *timer;
     //  自定义单元格Type的类型，在创建单元格的Item时使用
     enum    CellType{ctName=1000,ctSex,ctBirth,ctNation,ctPartyM,ctScore}; //各单元格的类型
     //  各字段在表格中的列号
@@ -38,7 +65,26 @@ private:
     QString pictrue_path;
     //picture display
     QGraphicsScene originalScene;
+    QPixmap pixmap;
+    //scope big
+    QRectF big_rect;
+
     int graphicsView_width;
     int graphicsView_height;
+    //display scope x and y
+    int edit_display_x1;
+    int edit_display_x2;
+    int edit_display_y1;
+    int edit_display_y2;
+    int edit_display_yz;
+    int edit_display_id;
+    int edit_display_X;
+    int edit_display_Y;
+    int edit_display_width;
+    int edit_display_height;
+
+    //src picture
+    cv::Mat src_picture;
+    cv::Mat process_mid_picture;
 };
 #endif // MAINWIDGET_H
